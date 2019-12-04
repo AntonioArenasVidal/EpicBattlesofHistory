3ea1a3f8df266265722b709cbee9fecc94c8671a
gustavovalery <43421832+gustavovalery@users.noreply.github.com>
2019-12-04 09:59:47 -0500

    Summon Needs DB Stuff and then Done

--- app/src/main/java/edu/fsu/cs/epicbattlesofhistory/SummonFragment.java
+++ app/src/main/java/edu/fsu/cs/epicbattlesofhistory/SummonFragment.java
@@ -7,10 +7,14 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -27,7 +31,7 @@ public class SummonFragment extends Fragment {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef;
 
-    Button backBtn;
+    ImageView character;
     Button summonBtn;
     TextView tokens;
 
@@ -39,15 +43,57 @@ public class SummonFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_summon, container, false);
+        setHasOptionsMenu(true);
         final View rootView = inflater.inflate(R.layout.fragment_summon, container, false);
         mListener = (SummonFragment.OnSummonFragmentInteractionListener) getActivity();
 
-        Random rand = new Random();
-        rand.setSeed(100);
+        character = (ImageView) rootView.findViewById(R.id.summon_character_image);
+        tokens = (TextView) rootView.findViewById(R.id.tokens);
+        summonBtn = (Button) rootView.findViewById(R.id.summon_button);
+        summonBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String summonedCharacter = "";
+                // TODO: Antonio randomly generate the character from DB based on the gold logic we were speaking off
+                Random rand = new Random();
+                rand.setSeed(100);
+                summonedCharacter = "arthur";
+                String characterImageName = summonedCharacter + "_character";
+
+                // TODO: Change token to corresponding token cound in users DB in firebase
+
+                tokens.setText(Integer.toString(Integer.parseInt(tokens.getText().toString()) - 1));
+                character.setImageResource(getResources().getIdentifier(characterImageName, "drawable", getContext().getPackageName()));
+            }
+        });
+
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_home_item:
+                mListener.onMenuMyHomeClicked();
+                break;
+            case R.id.menu_characters_item:
+                mListener.onMenuCharactersClicked();
+                break;
+            case R.id.menu_summon_item:
+                mListener.onMenuSummonClicked();
+                break;
+            case R.id.menu_logout_item:
+                mListener.onMenuLogoutClicked();
+                break;
+        }
+        return true;
     }
 
 
@@ -72,7 +118,10 @@ public class SummonFragment extends Fragment {
     public interface OnSummonFragmentInteractionListener {
         // TODO: Update argument type and name
         void onSummonCLicked();
-        //void backToMain();
-        //void onSummonClicked();
+        void onMenuMyHomeClicked();
+        void onMenuCharactersClicked();
+        void onMenuSummonClicked();
+        void onMenuLogoutClicked();
+        void onBattleClicked();
     }
 }
